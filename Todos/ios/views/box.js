04d1701dc172a2863c67d094be857b7d92b8dfea
slava hatnuke <slava.hatnuke@gmail.com>
2016-07-30 02:56:29 +0300

    wip

--- Todos/ios/views/box.js
+++ Todos/ios/views/box.js
@@ -7,12 +7,12 @@ import TodoList from './TodoList';
 import TodoEditor from './TodoEditor';
 import TodoApp from './TodoApp';
 
-box.add('Counter', ({CounterState}) => connect(CounterState)(Counter()));
+box.add('Counter', ({CounterState}) => connect(CounterState, Counter()));
 
 box.add('TodoApp', TodoApp);
 
-box.add('Todo', ({TodoState}) => connect(TodoState)(Todo()));
-box.add('TodoList', ({TodoListState, Todo}) => connect(TodoListState)(TodoList({Todo})));
-box.add('TodoEditor', ({TodoEditorState}) => connect(TodoEditorState)(TodoEditor()));
+box.add('Todo', ({TodoState}) => connect(TodoState, Todo()));
+box.add('TodoList', ({TodoListState, Todo}) => connect(TodoListState, TodoList({Todo})));
+box.add('TodoEditor', ({TodoEditorState}) => connect(TodoEditorState, TodoEditor()));
 
 export default box;
--- Todos/web/views/box.js
+++ Todos/web/views/box.js
@@ -11,8 +11,8 @@ box.add('Counter', ({CounterState}) => connect(CounterState)(Counter()));
 
 box.add('TodoApp', TodoApp);
 
-box.add('Todo', ({TodoState}) => connect(TodoState)(Todo()));
-box.add('TodoList', ({TodoListState, Todo}) => connect(TodoListState)(TodoList({Todo})));
-box.add('TodoEditor', ({TodoEditorState}) => connect(TodoEditorState)(TodoEditor()));
+box.add('Todo', ({TodoState}) => connect(TodoState, Todo()));
+box.add('TodoList', ({TodoListState, Todo}) => connect(TodoListState, TodoList({Todo})));
+box.add('TodoEditor', ({TodoEditorState}) => connect(TodoEditorState, TodoEditor()));
 
 export default box;
